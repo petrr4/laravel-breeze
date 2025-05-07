3bd319673ed36bccaaeea3ee6bcea7c5e3a1d258
petrr4 <petraik111@gmail.com>
2025-05-07 20:18:07 +0700

    addZ

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -148,27 +148,42 @@
         modalUpdate.classList.remove('hidden');
     }
 
+    <script>
     @if (Session::has('message'))
-    var type = "{{ Session::get('alert-type', 'info') }}";
-    switch (type) {
-        case 'info':
-            toastr.options.timeOut = 10000;
-            toastr.info("{{ Session::get('message') }}");
-            break;
-        case 'success':
-            toastr.options.timeOut = 10000;
-            toastr.success("{{ Session::get('message') }}");
-            break;
-        case 'warning':
-            toastr.options.timeOut = 10000;
-            toastr.warning("{{ Session::get('message') }}");
-            break;
-        case 'error':
-            toastr.options.timeOut = 10000;
-            toastr.error("{{ Session::get('message') }}");
-            break;
-    }
-@endif
+        var type = "{{ Session::get('alert-type', 'info') }}"
+        switch (type) {
+            case 'info':
+                toastr.options.timeOut = 10000;
+                toastr.info("{{ Session::get('message') }}");
+                var audio = new Audio('audio.mp3');
+                audio.play();
+                break;
+            case 'success':
 
+                toastr.options.timeOut = 10000;
+                toastr.success("{{ Session::get('message') }}");
+                var audio = new Audio('audio.mp3');
+                audio.play();
+
+                break;
+            case 'warning':
+
+                toastr.options.timeOut = 10000;
+                toastr.warning("{{ Session::get('message') }}");
+                var audio = new Audio('audio.mp3');
+                audio.play();
+
+                break;
+            case 'error':
+
+                toastr.options.timeOut = 10000;
+                toastr.error("{{ Session::get('message') }}");
+                var audio = new Audio('audio.mp3');
+                audio.play();
+
+                break;
+        }
+    @endif
+    </script>
     </script>
 </x-app-layout>
